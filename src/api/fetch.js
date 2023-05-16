3e53c8a9fcea3eaf4640d058e5c570eb477220fc
qiangyujun <qiangyujun@jd.com>
2023-05-16 17:21:37 +0800

    feat: 请求函数

--- src/api/fetch.js
+++ src/api/fetch.js
@@ -1,14 +1,24 @@
-import axios from "axios"
+import axios from "axios";
 
-const BASE_URL = ""
+const BASE_URL = "";
 
 const instance = axios.create({
-    baseURL: PRODUCT_URL,
-    timeout: 5000
-})
+  baseURL: PRODUCT_URL,
+  timeout: 5000,
+});
 
-export default function fetch(){
-    new Promise(() => {
-
-    })
+export default function fetch({ api, method = "post", data = {} }) {
+  return new Promise((resolve, reject) => {
+      instance({ api, method, data })
+          .then((res) => {
+              if (res.data.code === 200) {
+                resolve(res.data.data);
+              } else {
+                reject(res.data)
+            }
+          })
+          .catch((error) => {
+            reject(error.data);
+          })
+  });
 }
